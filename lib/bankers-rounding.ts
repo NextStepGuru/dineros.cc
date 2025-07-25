ba2f8d2a29576b3183e98757400c5d253a296095
Jeremy R DeYoung <jeremy@nextstep.guru>
2025-07-25 14:01:38 -0400

    Update bankers-rounding.ts

--- lib/bankers-rounding.ts
+++ lib/bankers-rounding.ts
@@ -1,5 +1,5 @@
 import bankersRounding from "bankers-rounding";
-import { Decimal } from "~/types/test-types";
+import { Decimal } from "@prisma/client/runtime/library";
 
 /**
  * Bankers Rounding Utility for Financial Calculations
